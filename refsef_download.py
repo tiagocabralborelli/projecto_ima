d1e9aeec9e89adaddd9618d61917d8870dfb9326
tiagocabralborelli <tiago.cabral422@gmail.com>
2022-06-07 10:14:20 -0300

    metadata table added

---
+++ refsef_download.py
@@ -0,0 +1,11 @@
+import os
+def genome_downloader(dir,fila:list):
+    """
+    Download genomes from RefSef
+    Parameter:
+        ->dir
+        ->
+    """
+
+
+    os.mkdir(os.path.join(os.getcwd(),dir))
